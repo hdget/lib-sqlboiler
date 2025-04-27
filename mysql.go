aa787dec2a95ece573fe44a27e96a186b5db2086
Ryan Fan <reg_info@qq.com>
2025-04-27 15:04:46 +0800

    add psql support (#2)

---
+++ mysql.go
@@ -0,0 +1,91 @@
+package sqlboiler
+
+import (
+	"fmt"
+	"github.com/hdget/utils/convert"
+	jsonUtils "github.com/hdget/utils/json"
+	reflectUtils "github.com/hdget/utils/reflect"
+	"github.com/volatiletech/sqlboiler/v4/queries/qm"
+	"reflect"
+)
+
+type MysqlImpl struct {
+}
+
+func Mysql() *MysqlImpl {
+	return &MysqlImpl{}
+}
+
+func (*MysqlImpl) IfNull(column string, defaultValue any, args ...string) string {
+	alias := column
+	if len(args) > 0 {
+		alias = args[0]
+	}
+
+	if defaultValue == nil {
+		return fmt.Sprintf("COALESCE((%s), '') AS \"%s\"", column, alias)
+	}
+
+	v := reflectUtils.Indirect(defaultValue)
+
+	switch vv := reflect.ValueOf(v); vv.Kind() {
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
+		return fmt.Sprintf("COALESCE((%s), %d) AS \"%s\"", column, v, alias)
+	case reflect.Float32, reflect.Float64:
+		return fmt.Sprintf("COALESCE((%s), %.4f) AS \"%s\"", column, v, alias)
+	case reflect.Slice:
+		if vv.Type().Elem().Kind() == reflect.Uint8 {
+			if jsonUtils.IsEmptyJsonObject(vv.Bytes()) {
+				return fmt.Sprintf("COALESCE((%s), '{}') AS \"%s\"", column, alias)
+			} else if jsonUtils.IsEmptyJsonArray(vv.Bytes()) {
+				return fmt.Sprintf("COALESCE((%s), '[]') AS \"%s\"", column, alias)
+			} else {
+				return fmt.Sprintf("COALESCE((%s), '%s') AS \"%s\"", column, convert.BytesToString(vv.Bytes()), alias)
+			}
+		}
+	}
+
+	return fmt.Sprintf("COALESCE((%s), '%v') AS \"%s\"", column, defaultValue, alias)
+}
+
+func (*MysqlImpl) IfNullWithColumn(column string, anotherColumn string, args ...string) string {
+	alias := column
+	if len(args) > 0 {
+		alias = args[0]
+	}
+	return fmt.Sprintf("IFNULL((%s), %s) AS \"%s\"", column, anotherColumn, alias)
+}
+
+func (*MysqlImpl) JsonValue(jsonColumn string, jsonKey string, defaultValue any) qm.QueryMod {
+	var template string
+	switch v := defaultValue.(type) {
+	case string:
+		template = fmt.Sprintf("IFNULL(JSON_UNQUOTE(JSON_EXTRACT(%s, '$.%s')), '%s') AS %s", jsonColumn, jsonKey, v, jsonKey)
+	case int8, int, int32, int64:
+		template = fmt.Sprintf("IFNULL(JSON_EXTRACT(%s, '$.%s'), %d) AS %s", jsonColumn, jsonKey, v, jsonKey)
+	case float32, float64:
+		template = fmt.Sprintf("IFNULL(JSON_EXTRACT(%s, '$.%s'), %f) AS %s", jsonColumn, jsonKey, v, jsonKey)
+	default:
+		return nil
+	}
+	return qm.Select(template)
+}
+
+func (*MysqlImpl) JsonValueCompare(jsonColumn string, jsonKey string, operator string, compareValue any) qm.QueryMod {
+	var template string
+	switch v := compareValue.(type) {
+	case string:
+		template = fmt.Sprintf("JSON_UNQUOTE(JSON_EXTRACT(%s, '$.%s')) %s '%s'", jsonColumn, jsonKey, operator, v)
+	case int8, int, int32, int64:
+		template = fmt.Sprintf("JSON_EXTRACT(%s, '$.%s') %s %d", jsonColumn, jsonKey, operator, v)
+	case float32, float64:
+		template = fmt.Sprintf("JSON_EXTRACT(%s, '$.%s') %s %f", jsonColumn, jsonKey, operator, v)
+	default:
+		return nil
+	}
+	return qm.Where(template)
+}
+
+func (impl *MysqlImpl) SUM(col string, args ...string) string {
+	return impl.IfNull(fmt.Sprintf("SUM(%s)", col), 0, args...)
+}
--- sqlboiler.go
+++ sqlboiler.go
@@ -3,13 +3,9 @@ package sqlboiler
 import (
 	"fmt"
 	"github.com/hdget/common/protobuf"
-	"github.com/hdget/utils/convert"
-	jsonUtils "github.com/hdget/utils/json"
 	"github.com/hdget/utils/paginator"
-	reflectUtils "github.com/hdget/utils/reflect"
 	"github.com/volatiletech/sqlboiler/v4/boil"
 	"github.com/volatiletech/sqlboiler/v4/queries/qm"
-	"reflect"
 	"time"
 )
 
@@ -17,46 +13,6 @@ import (
 func GetLimitQueryMods(list *protobuf.ListParam) []qm.QueryMod {
 	p := getPaginator(list)
 	return []qm.QueryMod{qm.Offset(int(p.Offset)), qm.Limit(int(p.PageSize))}
-}
-
-func IfNull(column string, defaultValue any, args ...string) string {
-	alias := column
-	if len(args) > 0 {
-		alias = args[0]
-	}
-
-	if defaultValue == nil {
-		return fmt.Sprintf("IFNULL((%s), '') AS \"%s\"", column, alias)
-	}
-
-	v := reflectUtils.Indirect(defaultValue)
-
-	switch vv := reflect.ValueOf(v); vv.Kind() {
-	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64, reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return fmt.Sprintf("IFNULL((%s), %d) AS \"%s\"", column, v, alias)
-	case reflect.Float32, reflect.Float64:
-		return fmt.Sprintf("IFNULL((%s), %.4f) AS \"%s\"", column, v, alias)
-	case reflect.Slice:
-		if vv.Type().Elem().Kind() == reflect.Uint8 {
-			if jsonUtils.IsEmptyJsonObject(vv.Bytes()) {
-				return fmt.Sprintf("IFNULL((%s), '{}') AS \"%s\"", column, alias)
-			} else if jsonUtils.IsEmptyJsonArray(vv.Bytes()) {
-				return fmt.Sprintf("IFNULL((%s), '[]') AS \"%s\"", column, alias)
-			} else {
-				return fmt.Sprintf("IFNULL((%s), '%s') AS \"%s\"", column, convert.BytesToString(vv.Bytes()), alias)
-			}
-		}
-	}
-
-	return fmt.Sprintf("IFNULL((%s), '%v') AS \"%s\"", column, defaultValue, alias)
-}
-
-func IfNullWithColumn(column string, anotherColumn string, args ...string) string {
-	alias := column
-	if len(args) > 0 {
-		alias = args[0]
-	}
-	return fmt.Sprintf("IFNULL((%s), %s) AS \"%s\"", column, anotherColumn, alias)
 }
 
 // WithUpdateTime 除了cols中的会更新以外还会更新更新时间字段
@@ -79,10 +35,6 @@ func GetDB(args ...boil.Executor) boil.Executor {
 		return args[0]
 	}
 	return boil.GetDB()
-}
-
-func SUM(col string, args ...string) string {
-	return IfNull(fmt.Sprintf("SUM(%s)", col), 0, args...)
 }
 
 func getPaginator(list *protobuf.ListParam) paginator.Paginator {
