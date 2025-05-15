8c4d16cb01383717a48130f151e5d3499dcadf2d
rfancn <reg_info@qq.com>
2025-05-15 18:15:16 +0800

    export escape

--- utils.go
+++ utils.go
@@ -6,13 +6,26 @@ import (
 	"strings"
 )
 
+const (
+	MysqlIdentifierQuote = "`"
+	PsqlIdentifierQuote  = "\""
+)
+
+func MysqlEscape(s string) string {
+	return escape(s, MysqlIdentifierQuote)
+}
+
+func PsqlEscape(s string) string {
+	return escape(s, PsqlIdentifierQuote)
+}
+
 func escape(s, quote string) string {
 	escaped := pie.Map(strings.Split(s, "."), func(v string) string {
-		return quoteString(v, quote)
+		return QuoteString(v, quote)
 	})
 	return strings.Join(escaped, ".")
 }
 
-func quoteString(s, quote string) string {
+func QuoteString(s, quote string) string {
 	return fmt.Sprintf("%s%s%s", quote, s, quote)
 }
