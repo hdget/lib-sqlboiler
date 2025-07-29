6c63dca43c2136e89b31ebed7cd1b0d3aeb16097
Ryan Fan <reg_info@qq.com>
2025-07-29 12:30:53 +0800

    fix escape issue (#9)

--- utils.go
+++ utils.go
@@ -1,14 +1,22 @@
 package sqlboiler
 
 import (
-	"fmt"
-	"github.com/elliotchance/pie/v2"
 	"strings"
 )
 
 func escape(s, quote string) string {
-	escaped := pie.Map(strings.Split(s, "."), func(v string) string {
-		return fmt.Sprintf("%s%s%s", quote, s, quote)
-	})
-	return strings.Join(escaped, ".")
+	// 1. 分割字符串
+	parts := strings.Split(s, ".")
+	// 2. 使用Builder高效构建
+	var builder strings.Builder
+	builder.Grow(len(s) + len(parts)*2) // 关键：预分配内存避免多次扩容
+	for i, p := range parts {
+		if i > 0 {
+			builder.WriteString(".") // 直接写入点号+双引号组合
+		}
+		builder.WriteString(quote)
+		builder.WriteString(p)
+		builder.WriteString(quote)
+	}
+	return builder.String()
 }
